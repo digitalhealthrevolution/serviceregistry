8d19dfc68ed2193f3d8f7f2fb2691bf7ee961f5a
dshub-dev <dev-team@digitalserviceshub.com>
2016-01-29 11:11:17 +0200

    Fix access token parsing error with malformatted credentials

--- serviceregistry-auth/src/main/java/fi/vtt/dsp/service/serviceregistry/auth/token/AccessTokenFactory.java
+++ serviceregistry-auth/src/main/java/fi/vtt/dsp/service/serviceregistry/auth/token/AccessTokenFactory.java
@@ -14,149 +14,157 @@ import org.apache.commons.lang.StringUtils;
 
 public final class AccessTokenFactory {
 
-	private static final Logger LOG = Logger.getLogger(AccessTokenFactory.class.getName());
+    private static final Logger LOG = Logger.getLogger(AccessTokenFactory.class.getName());
 
-	public static final String ADMIN_ID = "ADMIN-AGENT";
+    public static final String ADMIN_ID = "ADMIN-AGENT";
 
-	private final String SHARED_SECRET;
+    private final String SHARED_SECRET;
 
-	public AccessTokenFactory(String sharedSecret) {
-		this.SHARED_SECRET = sharedSecret;
-	}
+    public AccessTokenFactory(String sharedSecret) {
+        this.SHARED_SECRET = sharedSecret;
+    }
 
-	private String getAdminId(String id) {
-		return id + "_" + ADMIN_ID;
-	}
+    private String getAdminId(String id) {
+        return id + "_" + ADMIN_ID;
+    }
 
-	private String encode(String data) throws NoSuchAlgorithmException,
-			InvalidKeyException, IllegalStateException,
-			UnsupportedEncodingException {
+    private String encode(String data) throws NoSuchAlgorithmException,
+        InvalidKeyException, IllegalStateException,
+        UnsupportedEncodingException {
 
-		Mac sha256HMAC = Mac.getInstance("HmacSHA256");
-		SecretKeySpec key = new SecretKeySpec(SHARED_SECRET.getBytes("UTF8"), "HmacSHA256");
-		sha256HMAC.init(key);
-		return Base64.encodeBase64String(sha256HMAC.doFinal(data.getBytes("UTF8")));
-	}
+        Mac sha256HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec key = new SecretKeySpec(SHARED_SECRET.getBytes("UTF8"), "HmacSHA256");
+        sha256HMAC.init(key);
+        return Base64.encodeBase64String(sha256HMAC.doFinal(data.getBytes("UTF8")));
+    }
 
-	private String generateSignature(String id, AgentType type, AgentRole role) {
+    private String generateSignature(String id, AgentType type, AgentRole role) {
 
-		StringBuilder signatureBuilder = new StringBuilder();
+        StringBuilder signatureBuilder = new StringBuilder();
 
-		signatureBuilder.append(type);
-		signatureBuilder.append(":");
-		if (role.equals(AgentRole.ROLE_ADMIN)) {
-			signatureBuilder.append(getAdminId(id));
-		} else {
-			signatureBuilder.append(id);
-		}
-		String signature = null;
-		try {
-			signature = encode(signatureBuilder.toString());
-		} catch (InvalidKeyException | NoSuchAlgorithmException | IllegalStateException | UnsupportedEncodingException e) {
-			LOG.log(Level.SEVERE, "Error while generating access token signature: " + e.getMessage());
-		}
+        signatureBuilder.append(type);
+        signatureBuilder.append(":");
+        if (role.equals(AgentRole.ROLE_ADMIN)) {
+            signatureBuilder.append(getAdminId(id));
+        } else {
+            signatureBuilder.append(id);
+        }
+        String signature = null;
+        try {
+            signature = encode(signatureBuilder.toString());
+        } catch (InvalidKeyException | NoSuchAlgorithmException | IllegalStateException | UnsupportedEncodingException e) {
+            LOG.log(Level.SEVERE, "Error while generating access token signature: " + e.getMessage());
+        }
 
-		return signature;
-	}
+        return signature;
+    }
 
-	private String parseAgentId(String basicAuthUsername) {
-		return basicAuthUsername.substring(basicAuthUsername.indexOf('-') + 1);
-	}
+    private String parseAgentId(String basicAuthUsername) {
+        if (basicAuthUsername.contains("-")) {
+            return basicAuthUsername.substring(basicAuthUsername.indexOf('-') + 1);
+        } else {
+            return null;
+        }
+    }
 
-	private AgentType parseAgentType(String basicAuthUsername) {
-		String type = basicAuthUsername.substring(0, basicAuthUsername.indexOf('-'));
-		AgentType agentType = null;
-		try {
-			agentType = AgentType.valueOf(type.toUpperCase());
-		} 
-		catch(IllegalArgumentException e) {}
-		return agentType;
-	}
+    private AgentType parseAgentType(String basicAuthUsername) {
+        AgentType agentType = null;
+        if (basicAuthUsername.contains("-")) {
+            String type = basicAuthUsername.substring(0, basicAuthUsername.indexOf('-'));
+            try {
+                agentType = AgentType.valueOf(type.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                LOG.log(Level.SEVERE, "Error while parsing agent type: " + e.getMessage());
+            }
+        }
+        return agentType;
+    }
 
-	private AgentRole parseAgentRole(String agentId, AgentType agentType, String basicAuthPassword) {
-		String adminSignature = generateSignature(agentId, agentType, AgentRole.ROLE_ADMIN);
-		if (adminSignature.equals(basicAuthPassword)) {
-			return AgentRole.ROLE_ADMIN;
-		} else {
-			return AgentRole.ROLE_REGISTERED;
-		}
-	}
+    private AgentRole parseAgentRole(String agentId, AgentType agentType, String basicAuthPassword) {
+        String adminSignature = generateSignature(agentId, agentType, AgentRole.ROLE_ADMIN);
+        if (adminSignature.equals(basicAuthPassword)) {
+            return AgentRole.ROLE_ADMIN;
+        } else {
+            return AgentRole.ROLE_REGISTERED;
+        }
+    }
 
-	public AccessToken create(String id, AgentType type, AgentRole role) {
-		return new AccessTokenImpl(new Agent(id, type, role));
-	}
+    public AccessToken create(String id, AgentType type, AgentRole role) {
+        return new AccessTokenImpl(new Agent(id, type, role));
+    }
 
-	public AccessToken parse(String basicAuthUsername, String basicAuthPassword) {
+    public AccessToken parse(String basicAuthUsername, String basicAuthPassword) {
 
-		AccessToken token = null;
-		if(StringUtils.isBlank(basicAuthUsername) || StringUtils.isBlank(basicAuthPassword)) {
-			token = new AccessTokenImpl(null, null);
-		} else {
-			String agentId = parseAgentId(basicAuthUsername);
-			AgentType agentType = parseAgentType(basicAuthUsername);
-			AgentRole agentRole = parseAgentRole(agentId, agentType, basicAuthPassword);
-			if( agentId != null && agentType != null && agentRole != null ) {
-				Agent agent = new Agent(agentId, agentType, agentRole);	
-				token = new AccessTokenImpl(agent, basicAuthPassword);
-			} else {
-				token = new AccessTokenImpl(null, null);				
-			}
-		}
-		return token;
-	}
-	
-	private class AccessTokenImpl implements AccessToken {
+        AccessToken token = null;
+        if (StringUtils.isBlank(basicAuthUsername) || StringUtils.isBlank(basicAuthPassword)) {
+            token = new AccessTokenImpl(null, null);
+        } else {
+            String agentId = parseAgentId(basicAuthUsername);
+            AgentType agentType = parseAgentType(basicAuthUsername);
+            AgentRole agentRole = parseAgentRole(agentId, agentType, basicAuthPassword);
+            if (agentId != null && agentType != null && agentRole != null) {
+                Agent agent = new Agent(agentId, agentType, agentRole);
+                token = new AccessTokenImpl(agent, basicAuthPassword);
+            } else {
+                token = new AccessTokenImpl(null, null);
+            }
+        }
+        return token;
+    }
 
-		private final Agent agent;
-		private final String signature;
-	
-		public AccessTokenImpl(Agent agent) {
-			this.agent = agent;
-			this.signature = generateSignature(agent.getId(), agent.getType(), agent.getRole());			
-		}
-		public AccessTokenImpl(Agent agent, String signature) {
-			this.agent = agent;
-			this.signature = signature;
-		}
-		
-		@Override
-		public String getBasicAuthUsername() {
+    private class AccessTokenImpl implements AccessToken {
 
-			if (this.agent == null) {
-				return null;
-			}
+        private final Agent agent;
+        private final String signature;
 
-			StringBuilder builder = new StringBuilder();
-			builder.append(this.agent.getType().toString().toLowerCase());
-			builder.append("-");
-			builder.append(this.agent.getId());
-			return builder.toString();
-		}
+        public AccessTokenImpl(Agent agent) {
+            this.agent = agent;
+            this.signature = generateSignature(agent.getId(), agent.getType(), agent.getRole());
+        }
 
-		@Override
-		public String getBasicAuthPassword() {
-			return this.signature;
-		}
+        public AccessTokenImpl(Agent agent, String signature) {
+            this.agent = agent;
+            this.signature = signature;
+        }
 
-		@Override
-		public boolean isValid() {
-			return (this.agent != null && !StringUtils.isEmpty(this.signature));
-		}
+        @Override
+        public String getBasicAuthUsername() {
 
-		@Override
-		public boolean isAuthentic() {
-			if (this.isValid()) {
-				String expectedSignature = generateSignature(agent.getId(), agent.getType(), agent.getRole());
-				return expectedSignature.equals(this.signature);
-			} else {
-				return false;
-			}
-		}
+            if (this.agent == null) {
+                return null;
+            }
 
-		@Override
-		public Agent getAgent() {
-			return agent;
-		}
-	}
+            StringBuilder builder = new StringBuilder();
+            builder.append(this.agent.getType().toString().toLowerCase());
+            builder.append("-");
+            builder.append(this.agent.getId());
+            return builder.toString();
+        }
+
+        @Override
+        public String getBasicAuthPassword() {
+            return this.signature;
+        }
+
+        @Override
+        public boolean isValid() {
+            return (this.agent != null && !StringUtils.isEmpty(this.signature));
+        }
+
+        @Override
+        public boolean isAuthentic() {
+            if (this.isValid()) {
+                String expectedSignature = generateSignature(agent.getId(), agent.getType(), agent.getRole());
+                return expectedSignature.equals(this.signature);
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public Agent getAgent() {
+            return agent;
+        }
+    }
 
 }
